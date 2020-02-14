d79ded6b7ed25969d6a6bf33353ff8d3038e14d6
resjudicoda <adam.j.reis@gmail.com>
2020-02-13 21:38:25 -0600

    finally got map working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('testing');
+
+const mapboxgl = require("mapbox-gl");
+
+//const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmVzanVkaWNvZGEiLCJhIjoiY2s2bGwydWlvMGdiaDNscHVtbGM2cGp2NiJ9.SlihU79Gq84SrdM9xxYTXA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-74.009, 40.705] for NY
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
